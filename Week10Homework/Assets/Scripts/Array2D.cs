a2874a54b03f4b93e5557223fd6c9748cd96c24a
Alessia Ianni-Palarchio <alessia@mipsscan.com>
2021-04-10 10:45:25 -0400

    First Commit

---
+++ Week10Homework/Assets/Scripts/Array2D.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array2D : MonoBehaviour
+{
+    private Vector2[,] tilePlaces;
+    private GameObject[,] tilesAtPos;
+
+    public int height;
+    public int width;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MakeGrid()
+    {
+        tilePlaces = new Vector2[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var newPos = new Vector2(x, y);
+                Debug.Log("Position at " + x + "," + y + " is" + newPos);
+                tilePlaces[x, y] = newPos;
+            }
+        }
+    }
+    
+}
